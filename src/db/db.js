c55610b77760d2cc8e71de9cb9276fcb94a365f1
Suraj Telrandhe <suraj.telrandhe16@gmail.com>
2024-06-08 18:27:04 +0530

    utils coded

--- src/db/db.js
+++ src/db/db.js
@@ -8,7 +8,7 @@ const connectDB = async () => {
         console.log(`\n MongoDB connected !! DB HOST:${connectionInstance.connection.host}`);
         // console.log(connectionInstance);
     } catch (error) {
-        console.log("MONGODB CONNECTION ERROR:", error);
+        console.log("MONGODB CONNECTION FAILED:", error);
         process.exit(1);
     }
 };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import dotenv from "dotenv";
 import connectDB from "./db/db.js";
+import app from "./app.js";
 
 dotenv.config({
     path: "./env",
 });
 
 connectDB()
+    .then(() => {
+        app.on("error", (error) => {
+            console.log("ERROR: ", error);
+        })
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running on port ${process.env.PORT || 8000}`);
+        })
+    })
+    .catch((err) => {
+        console.log("MONGODB CONNECTION FAILED !! ", err);
+    })
 
 
 
